d4a9f0151d2a6f57caddc933bc39a465b1dfdec9
hanlee <me@hanlee.co>
2020-08-15 03:37:47 +0900

    Styled List

--- client/src/components/Todo.vue
+++ client/src/components/Todo.vue
@@ -1,20 +1,28 @@
 <template>
-  <h1>Todo List gRPC Client</h1>
-  <div>
-    <input type="text">
-    <button>Add Todo</button>
+  <div class="w-1/4 mx-auto pt-20 text-center">
+    <h1 class="text-2xl text-center font-bold">Todo List gRPC Client</h1>
+    <div class="mt-10">
+      <input type="text" v-model="inputField" class="appearance-none border rounded py-2 px-3 text-black mb-3 leading-tight outline-none bg-gray-400">
+      <button class="btn ml-4" @click="addTodo(inputField)" :disabled="!inputField">Add Todo</button>
+    </div>
+    <ul class="flex flex-col mt-6 rounded-lg border divide-y divide-gray-400">
+      <li class="block p-4 border-grey flex items-stretch" v-for="todo in todos" :key="todo.id">
+        <p class="flex-1">{{todo.task}}</p>
+        <div class="flex-none h-6 w-6 flex items-center justify-center hover:text-gray-600" @click="deleteTodo(todo)">
+          <CloseIcon />
+        </div>
+      </li>
+    </ul>
   </div>
-  <ul class=" list-reset flex flex-col">
-    <li class=" rounded-t relative -mb-px block border p-4 border-grey">Cras justo odio</li>
-	</ul>
-  {{ todos }}
 </template>
 
 <script>
 import { ref } from 'vue'
 import { getTodoParams, addTodoParams, deleteTodoParams } from "../todo_pb.js";
 import { todoServiceClient } from "../todo_grpc_web_pb.js";
+import CloseIcon from './CloseIcon'
 export default {
+  components:{ CloseIcon },
   setup() {
     const todos = ref([])
     const inputField = ref('')
@@ -25,6 +33,7 @@ export default {
       let getRequest = new getTodoParams();
       client.getTodos(getRequest, {}, (err, response) => {
         if (err) console.log(err);
+        console.log(response.toObject());
         todos.value = response.toObject().todosList;
       });
     }
@@ -33,7 +42,7 @@ export default {
 
     const addTodo = () => {
       let addRequest = new addTodoParams();
-      addRequest.setTask(inputField);
+      addRequest.setTask(inputField.value);
       client.addTodo(addRequest, {}, (err) => {
         if (err) console.log(err);
         inputField.value = "";
@@ -59,10 +68,4 @@ export default {
     }
   }
 }
-
-
-
-
-
-
 </script>
